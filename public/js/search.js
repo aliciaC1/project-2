0064bea4b29127ee4a9931b3a2a88c7fe8a335c9
Alicia <chan.aliciaw@gmail.com>
2018-10-05 12:02:35 -0400

    added logic to pick up select values

--- public/js/search.js
+++ public/js/search.js
@@ -1,29 +1,13 @@
-    $('#searchResult').on('click', function (event) {
-        event.preventDefault();
-        var userInput = {
-            location: $('#location-select').dropdown('get value'), 
-            product:$('#product-select').dropdown('get value')
-        };
-        var newURL = "/example/"+ userInput.location + "/" + userInput.product;
-        alert(newURL);
-            $('<a>').attr("href", newURL);
-            return true;
-           
-    }); 
-    
+// $('#searchResult').on('click', function (event) {
+//     event.preventDefault();
+//     var userInput = {
+//         location: $('#location-select').dropdown('get value'), 
+//         product:$('#product-select').dropdown('get value')
+//     };
+//     var newURL = "/example/"+ userInput.location + "/" + userInput.product;
+//     alert(newURL);
+//         $('<a>').attr("href", newURL);
+//         return true;
+       
+// }); 
 
-// });
-    
-//     $("#clickThis").click(function() {
-//         $("#testNext").attr("href",newURL);
-//         console.log(newURL);
-//     });
-//     //$submit.on("click", handleSubmission);
-
-
-// // Get references to page elements
-// $(document).ready(function() {
-//     // var $locationID = $("#NewYork").attr("valueNumber");
-//     // var $productID = $("#tshirt").attr("valueNumber");
-//     //var $submit=$(".submit");
-    
